75ec252018b9262fe8950af048c82fbdab9b2223
2424005179@qq.com <2424005179@qq.com>
2024-10-18 23:52:56 +0800

    feat: Home组件

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Top } from './style';
 function Home(props) {
+
   return (
-    <div>Home</div>
+    <div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+    </div>
+
   )
 }
 
