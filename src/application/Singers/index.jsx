f31eecd4b832b582e036ce394f5dd4045766fa3d
2424005179@qq.com <2424005179@qq.com>
2025-01-09 19:54:10 +0800

    feat: 滚动问题待解决

---
+++ src/application/Singers/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes } from '../../api/config';
+function Singers(props) {
+  return (
+    <Horizen list={categoryTypes} title={"分类 (默认热门):"}></Horizen>
+  )
+}
+
+export default React.memo(Singers)
