6acacbbed9646bf0520b36cb22adf59b6ac88806
2424005179@qq.com <2424005179@qq.com>
2025-01-14 18:55:02 +0800

    feat: 歌手列表2

--- src/api/request.js
+++ src/api/request.js
@@ -4,6 +4,14 @@ export const getBannerRequest = () => {
     return axiosInstance.get('/banner')
 }
 
-export const getRecommendListRequest  = () => {
+export const getRecommendListRequest = () => {
     return axiosInstance.get('/personalized')
+}
+
+export const getHotSingerListRequest = (count) => {
+    return axiosInstance.get(`/top/artists?offset=${count}`);
+}
+
+export const getSingerListRequest = (category, alpha, count) => {
+    return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
 }
--- src/application/Singers/index.jsx
+++ src/application/Singers/index.jsx
@@ -1,31 +1,126 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '../../api/config';
-import { NavContainer } from "./style";
+import { 
+  NavContainer,
+  ListContainer,
+  List,
+  ListItem
+} from "./style";
+import Scroll from "../../baseUI/scroll/index";
+import { 
+  getSingerList, 
+  getHotSingerList, 
+  changeEnterLoading, 
+  changePageCount, 
+  refreshMoreSingerList, 
+  changePullUpLoading, 
+  changePullDownLoading, 
+  refreshMoreHotSingerList 
+} from './store/actionCreators';
+import {connect} from 'react-redux';
 function Singers(props) {
 
   let [category, setCategory] = useState('')
   let [alpha, setAlpha] = useState('')
+
+  const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
+    return {
+      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "隔壁老樊",
+      accountId: 277313426,
+    }
+  })
 
   let handleUpdateAlpha = (val) => {
     setAlpha(val)
   }
 
   let handleUpdateCategory = (val) => {
-    setCategory (val);
+    setCategory(val);
+  }
+
+  const renderSingerList = () => {
+    return (
+      <List>
+        {
+          singerList.map((item, index) => {
+            return (
+              <ListItem key={item.accountId + "" + index}>
+                <div className="img_wrapper">
+                  <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+
+      </List>
+
+    )
   }
 
   return (
+    <div>
+      {/* better-scroll 的 (横向) 滚动原理，首先外面容器要宽度固定，其次内容宽度要大于容器宽度。 */}
+      <NavContainer>
+        <Horizen list={categoryTypes} title={"分类 (默认热门):"}
+          handleClick={handleUpdateCategory} oldVal={category}></Horizen>
+        <Horizen list={alphaTypes} title={"首字母:"}
+          handleClick={handleUpdateAlpha} oldVal={alpha}></Horizen>
+      </NavContainer>
+      <ListContainer>
+        <Scroll>
+          {renderSingerList()}
+        </Scroll>
+      </ListContainer>
+    </div>
 
-    // better-scroll 的 (横向) 滚动原理，首先外面容器要宽度固定，其次内容宽度要大于容器宽度。
-    <NavContainer>
-      <Horizen list={categoryTypes} title={"分类 (默认热门):"}
-      handleClick={handleUpdateCategory} oldVal={category}></Horizen>
-      <Horizen list={alphaTypes} title={"首字母:"}
-      handleClick={handleUpdateAlpha} oldVal={alpha}></Horizen>      
-    </NavContainer>
+
 
   )
 }
+// 映射redux的state到组件的state上
+const mapStateToProps = (state) => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount'])
+}) 
 
-export default React.memo(Singers)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+      dispatch(changeEnterLoading(true));//loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+      dispatch(getSingerList(category, alpha));
+    },
+    // 滑到最底部刷新部分的处理
+    pullUpRefreshDispatch(category, alpha, hot, count) {
+      dispatch(changePullUpLoading(true));
+      dispatch(changePageCount(count+1));
+      if(hot){
+        dispatch(refreshMoreHotSingerList());
+      } else {
+        dispatch(refreshMoreSingerList(category, alpha));
+      }
+    },
+    //顶部下拉刷新
+    pullDownRefreshDispatch(category, alpha) {
+      dispatch(changePullDownLoading(true));
+      dispatch(changePageCount(0));//属于重新获取数据
+      if(category === '' && alpha === ''){
+        dispatch(getHotSingerList());
+      } else {
+        dispatch(getSingerList(category, alpha));
+      }
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers))
