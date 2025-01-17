1458fe7e1b3d3eb42916298831c6bcfd4807c8bd
17722077220 <2424005179@qq.com>
2025-01-18 00:58:40 +0800

    feat: 排行榜List样式待开发

---
+++ src/application/Rank/index.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux';
+import { getRankList } from './store'
+import { filterIndex } from '../../api/utils'
+import Loading from '../../baseUI/loading';
+import { EnterLoading } from '../Singers/style';
+import Scroll from '../../baseUI/scroll/index';
+import {
+  Container
+} from './style';
+
+
+function Rank(props) {
+  const { rankList: list, loading } = props;
+  const { getRankListDataDispatch } = props;
+
+  let rankList = list ? list.toJS() : []
+
+  useEffect(() => {
+    if (!rankList.length) {
+      getRankListDataDispatch()
+    }
+  })
+
+  let globalStartIndex = filterIndex(rankList);
+  let officialList = rankList.slice(0, globalStartIndex);
+  let globalList = rankList.slice(globalStartIndex);
+
+  // 榜单数据未加载出来之前都给隐藏
+  let displayStyle = loading ? { "display": "none" } : { "display": "" };
+  // 这是渲染榜单列表函数，传入 global 变量来区分不同的布局方式
+
+  const renderSongList = (list) => {
+    return list.length ? (
+      <songList>
+        {
+          list.map((item, index) => {
+            return <li key={index}>{index + 1}. {item.first} - {item.second}</li>
+          })
+        }
+      </songList>
+    ) : null
+  }
+  // const renderRankList = (list, global) => {
+  //   return (
+  //     <List globalRank={global}>
+  //       {
+  //         list.map((item) => {
+  //           return (
+  //             <ListItem key={item.coverImgId} tracks={item.tracks} onClick={() => enterDetail(item.name)}>
+  //               <div>
+  //                 <img src={img.coverImgUrl} />
+  //                 <div></div>
+  //                 <span>{item.updateFrequency}</span>
+  //               </div>
+  //               {renderSongList(item.tracks)}
+  //             </ListItem>
+  //           )
+  //         })
+  //       }
+
+  //     </List>
+  //   )
+
+  // }
+  return (
+    <Container>
+      <Scroll>
+        <div>
+          <h1 className="offical" style={displayStyle}> 官方榜 </h1>
+          {/* {renderRankList(officialList)} */}
+          <h1 className="global" style={displayStyle}> 全球榜 </h1>
+          {/* {renderRankList(globalList, true)} */}
+          {loading ? <EnterLoading><Loading></Loading></EnterLoading> : null}
+        </div>
+      </Scroll>
+    </Container>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  rankList: state.getIn(['rank', 'rankList']),
+  loading: state.getIn(['rank', 'loading']),
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getRankListDataDispatch() {
+      dispatch(getRankList())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Rank))
