11364ecf4fe80fb64a9818dc5b60110877680546
17722077220 <2424005179@qq.com>
2025-01-21 01:03:09 +0800

    feat: 歌单详情2

--- src/application/Album/index.jsx
+++ src/application/Album/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from './style'
+import { CSSTransition } from 'react-transition-group'
+import { useNavigate } from 'react-router-dom';
+import Header from '../../baseUI/header/index'
 
 function Album(props) {
+  const navigate = useNavigate();
+  const [showStatus, setShowStatus] = useState(true)
+  const handleBack = () => {
+    setShowStatus (false);
+  };
     return (
+      <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
+      unmountOnExit
+      onExit={() => navigate(-1)}>
         <Container>
+        <Header title={"返回"} handleClick={handleBack}></Header>
         </Container>
+
+      </CSSTransition>
+
       )
 }
-export default Album
+export default React.memo(Album)
